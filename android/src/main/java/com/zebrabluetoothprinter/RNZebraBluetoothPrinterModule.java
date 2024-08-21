9b762cc198ca7880977b1f57d7ee0b9467c32f0d
damiantokarczyk <155967661+damiantokarczyk@users.noreply.github.com>
2024-08-21 12:59:02 +0200

    Update RNZebraBluetoothPrinterModule.java

--- android/src/main/java/com/zebrabluetoothprinter/RNZebraBluetoothPrinterModule.java
+++ android/src/main/java/com/zebrabluetoothprinter/RNZebraBluetoothPrinterModule.java
@@ -39,7 +39,8 @@ import java.util.List;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.zebra.sdk.comm.BluetoothConnection;                                  // using zebra sdk for print functionality
+import com.zebra.sdk.btleComm.BluetoothLeConnection
+//import com.zebra.sdk.comm.BluetoothConnection;
 import com.zebra.sdk.comm.Connection;
 import com.zebra.sdk.printer.PrinterStatus;
 import com.zebra.sdk.comm.ConnectionException;
@@ -479,7 +480,7 @@ public class RNZebraBluetoothPrinterModule extends ReactContextBaseJavaModule im
   public void print(String device, String label,final Promise promise) {            //print functionality for zebra printer
     boolean success = false;
     boolean loading = false;
-    connection = new BluetoothConnection(device);
+    connection = new BluetoothLeConnection(device);
     try {
       loading = true;
       connection.open();
